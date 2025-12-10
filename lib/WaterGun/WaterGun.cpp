2db5de126f002b5615b8292c70852b86ccdde6a1
jrobles686 <jroblescollege686@gmail.com>
2025-12-09 16:38:38 -0800

    Update WaterGun.cpp

--- lib/WaterGun/WaterGun.cpp
+++ lib/WaterGun/WaterGun.cpp
@@ -32,3 +32,52 @@ namespace WaterGun {
     }
 
 }
+
+// Hey Gabe, lmk if this is what you are looking for.
+// If it is, then we can delete the above code. 
+
+#include "WaterGun.h"
+#include <Arduino.h>  
+
+namespace WaterGun {
+
+    // Arduino pin numbers 
+    const int FWD_PIN  = 22;  // PA0
+    const int BACK_PIN = 23;  // PA1
+
+    // Bit positions on PORTA
+    constexpr uint8_t FWD_BIT  = PA0;  // bit 0
+    constexpr uint8_t BACK_BIT = PA1;  // bit 1
+
+    bool shot = false;
+
+    void setup() {
+        // Set PA0 and PA1 as outputs: DDRA |= (1 << PA0) | (1 << PA1);
+        DDRA |= _BV(FWD_BIT) | _BV(BACK_BIT);
+
+        // Initialize outputs:
+        // FWD low, BACK high
+        PORTA &= ~_BV(FWD_BIT);   // FWD = LOW
+        PORTA |=  _BV(BACK_BIT);  // BACK = HIGH
+    }
+
+    bool shoot() {
+        if (!shot) {
+
+            // both are low to avoid shorts
+            PORTA &= ~(_BV(BACK_BIT) | _BV(FWD_BIT));  // BACK = LOW, FWD = LOW
+
+            unsigned long startTime = millis();
+            while (millis() - startTime < 500) {
+                // busy-wait 500 ms
+            }
+
+            // Drive forward line HIGH
+            PORTA |= _BV(FWD_BIT);
+
+            shot = true;
+        }
+        return shot;
+    }
+
+}
